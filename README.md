0198fe44257e7689efe5fcfda84057236b593166
Joel-Sempere-Cobos <91223315+Joel-Sempere-Cobos@users.noreply.github.com>
2023-10-15 14:54:52 +0200

    Youtube unblocker

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # adblockBlockerBlocker
+
 Chrome extension to block the adblock blockers of some websites
+
+# List of websites supported:
+
+- YouTube
---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tabId },
+        files: ['./adblockUnblocker.js'],
+      })
+      .then(() => {
+        console.log('INJECTED THE FOREGROUND SCRIPT.');
+      })
+      .catch((err) => console.log(err));
+  }
+});
