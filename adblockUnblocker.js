bfbc2124deab331294c9e74f3de335c17f7474c2
Joel-Sempere-Cobos <91223315+Joel-Sempere-Cobos@users.noreply.github.com>
2023-10-16 11:16:35 +0200

    Fix

--- adblockUnblocker.js
+++ adblockUnblocker.js
@@ -1,6 +1,6 @@
 // YouTube
-const overlay = document.querySelector('tp-yt-iron-overlay-backdrop');
+let overlay = document.querySelector('tp-yt-iron-overlay-backdrop');
 overlay?.remove();
 
-const popup = document.querySelector('ytd-enforcement-message-view-model');
+let popup = document.querySelector('ytd-enforcement-message-view-model');
 popup?.remove();
